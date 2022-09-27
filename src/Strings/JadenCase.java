ae8469576250b62f7e027061229aa0b8005ddcbb
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-09-26 21:11:23 -0400

    Java Tasks

---
+++ src/Strings/JadenCase.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class JadenCase {
+    public static void main(String[] args) {
+
+        System.out.println(toJadenCase2("hi my name is ower"));
+    }
+
+    public static String toJadenCase2(String givenSentence) {
+        if (givenSentence == null || givenSentence.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+        String [] words = givenSentence.split(" ");
+
+        for (String str : words){
+
+            result.append((str.charAt(0) + "").toUpperCase())
+                  .append(str.substring(1))
+                  .append(" ");
+
+        }
+
+        return result.toString().trim();
+    }
+
+}
