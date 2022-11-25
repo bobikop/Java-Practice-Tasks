bfce9159913d0b82f1b265cca4e87b0b5f953fcf
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-11-25 17:04:13 -0500

    Java Algorithms

---
+++ src/NumberTasks/Number_Armstrong.java
@@ -0,0 +1,29 @@
+package NumberTasks;
+
+public class Number_Armstrong {
+
+
+    /*
+    Write a method that can check if a number is Armstrong number
+     */
+
+    public static boolean isArmStrongNumber(int num) {
+        int a = 0, b = 0, c = num;
+
+        while (num > 0) {
+            a = num % 10;
+            num = num /10;
+            b += (a * a * a);
+        }
+
+        return (c == b) ? true : false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(isArmStrongNumber(371));
+    }
+}
