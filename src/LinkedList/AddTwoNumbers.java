1c1f3818b06dde869fda620482a59cb42f646afc
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2022-11-25 22:34:55 -0500

    Java Algorithms

---
+++ src/LinkedList/AddTwoNumbers.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+import LinkedList.PalindromeLinkedList.ListNode;
+
+public class AddTwoNumbers {
+
+  static class ListNode{
+
+      int val;
+      ListNode next;
+
+      public ListNode(int val) {
+          this.val = val;
+      }
+  }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+         while (l1 != null || l2 != null || carry != 0){
+
+             int x = (l1 != null) ? l1.val : 0;
+             int y = (l2 != null)? l2.val : 0;
+             int sum = carry + x+y;
+             carry = sum / 10;
+
+             current.next = new ListNode(sum % 10);
+             current = current.next;
+
+             if (l1 != null){
+                 l1=l1.next;
+             }
+             if (l2 != null){
+                 l2=l2.next;
+             }
+         }
+
+         return dummy.next;
+
+    }
+}
