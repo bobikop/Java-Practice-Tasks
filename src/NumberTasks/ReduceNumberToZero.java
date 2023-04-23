adef34200a735e889378db32ff2bb0d532cf1e79
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-04-23 19:20:26 -0400

    Practice

--- src/NumberTasks/ReduceNumberToZero.java
+++ src/NumberTasks/ReduceNumberToZero.java
@@ -6,11 +6,10 @@ public class ReduceNumberToZero {
     public static void main(String[] args) {
          int num = 8;
         System.out.println(NumberToZero(num));
-
-
+        System.out.println(NumberToZero1(num));
     }
 
-
+    //solution 1
     public static int NumberToZero(int num){
 
 
@@ -25,6 +24,24 @@ public class ReduceNumberToZero {
       }
       return count;
     }
+    //solution 2 (using bitwise operator)
+
+    public static int NumberToZero1(int num){
+
+
+        int count = 0;
+        while(num > 0){
+            if((num & 1) == 0){
+                num >>= 1;
+            }else{
+                num -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+
+
 }
 // Time Complexity = O(logn)
 //Space complexity = O(1)
