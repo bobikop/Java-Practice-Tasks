0773c3deb01edd4c15240c0c0c62eb1c9a28b754
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-10 19:05:35 -0400

    Binary Search Algorithm

--- src/NumberTasks/BinarySearch/BinarySearch.java
+++ src/NumberTasks/BinarySearch/BinarySearch.java
@@ -1,10 +1,18 @@
 package NumberTasks.BinarySearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 
     // find if the element is present in the sorted array
 
     public static void main(String[] args) {
+        int[] ints = {1,2,3,4,5,6,7,9,11};
+
+       int result =  binarySearch(ints,9);
+        System.out.println(result);
+        // :) using Java library we can use
+        System.out.println( Arrays.binarySearch(ints, 9));
 
     }
 
