f3ed714d976f92e72fbedf49aa3605004bc17e4a
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-06-10 18:51:53 -0400

    Greatest Common Divisor

--- src/NumberTasks/BinarySearch/BinarySearch.java
+++ src/NumberTasks/BinarySearch/BinarySearch.java
@@ -4,6 +4,15 @@ public class BinarySearch {
 
     // find if the element is present in the sorted array
 
+    public static void main(String[] args) {
 
+    }
+
+
+    private static int binarySearch(int[] numbers, int numberToFind){
+
+
+
+    }
 
 }
