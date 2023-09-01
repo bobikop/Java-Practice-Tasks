882e4135a2ca2aaf3283cc66586a636275d3937d
Boban Milosevic <104642358+bobikop@users.noreply.github.com>
2023-09-01 10:20:58 -0400

    Test

---
+++ src/Stack/Test1.java
@@ -0,0 +1,15 @@
+package Stack;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(6,8));
+
+    }
+    public static int sum(int num1,int num2){
+        return num1+num2;
+
+    }
+
+}
